19840ead21012582b05c36c64c14c5c10b9855a8
Wanderson Mauricio <wandersonsilvamauricio01@gmail.com>
2023-09-25 10:21:45 -0300

    Atualização no codigo javascript

--- src/script.js
+++ src/script.js
@@ -1,19 +1,16 @@
 
-const botaoGerador = document.querySelector(".advice-update").addEventListener("click", geradorDeConselhos); 
-const idConselho = document.querySelector(".advice-id");
-const descriptionConselho = document.querySelector(".advice-description");
+const btn = document.querySelector(".advice-update").addEventListener("click", geradorDeConselho);
+const adviceId = document.querySelector(".advice-id");
+const adviceDescription = document.querySelector(".advice-description");
 
-async function geradorDeConselhos() {
-     const urlGerador = await fetch("https://api.adviceslip.com/advice");
-     const resposta = await urlGerador.json();
-     const adviceId = `Advice# ${resposta.slip.id}`
-     const adviceUpdat = `"${resposta.slip.advice}"`
-
-
-     idConselho.innerHTML = adviceId
-     descriptionConselho.innerHTML = adviceUpdat
-};
-geradorDeConselhos();
+async function geradorDeConselho() {
+     const urlApi = await fetch("https://api.adviceslip.com/advice");
+     const resultado = await urlApi.json()
+     const resultadoId = `Advice# ${resultado.slip.id}`
+     const resultadoDescription = `"${resultado.slip.advice}"`
 
 
-
+     adviceId.innerHTML = resultadoId
+     adviceDescription.innerHTML = resultadoDescription
+}
+geradorDeConselho()
